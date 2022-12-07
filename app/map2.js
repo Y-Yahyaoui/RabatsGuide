7e27fd1792b030ee3441aa6b696eec4127adfd82
Yusso <yahyaouiyoussef@iav.ac.ma>
2022-12-07 02:20:30 +0100

    f it

--- app/map2.js
+++ app/map2.js
@@ -1,25 +1,23 @@
 var map = L.map('map').setView([34.0255, -6.833], 16); 
 
 new L.basemapsSwitcher([
-    {
-      layer: L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map), //DEFAULT MAP
-      icon: 'SwitchBasemap/example/assets/images/img1.PNG',
-      name: 'Open street'
-    },
-    {
-      layer: L.tileLayer('https://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}'),
-      icon: 'SwitchBasemap/example/assets/images/img2.PNG',
-      name: 'Maps'
-    },
-    {
-      layer: L.tileLayer('https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}'),
-      icon: 'SwitchBasemap/example/assets/images/sat.PNG',
-      name: 'Satellite'
-    },
-    
-  ], { position: 'topright' }).addTo(map);
+  {
+    layer: L.tileLayer('https://api.mapbox.com/styles/v1/nafissa1809/clbcvdaxm002914o0nj4d8kc0/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibmFmaXNzYTE4MDkiLCJhIjoiY2xiYTFtZDJkMTBlZjNxcWh1aHdwbnp0aCJ9.OBCk9K8H_5L_JfgvBgT8jQ', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map), //DEFAULT MAP
+    icon: 'SwitchBasemap/example/assets/images/img1.PNG',
+    name: 'Map box'
+  },
+  {
+    layer: L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'),
+    icon: 'SwitchBasemap/example/assets/images/img2.PNG',
+    name: 'Maps'
+  },
+  {
+    layer: L.tileLayer('http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}'),
+    icon: 'SwitchBasemap/example/assets/images/sat.PNG',
+    name: 'Satellite'
+  }], { position: 'topright' }).addTo(map);
 
   		// create fullscreen control
           var fsControl = L.control.fullscreen();
